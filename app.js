f445a213e9959bc8ce7d37e516c7a670330ff0be
tevinowino <tevinowino65@gmail.com>
2024-10-22 10:35:48 +0300

    Express App

---
+++ app.js
@@ -0,0 +1,54 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var { MongoClient, ObjectId } = require('mongodb'); // Import MongoClient and ObjectId
+
+require('dotenv').config();
+
+console.log(process.env.MONGO_PW); // Check if the password is loaded from .env
+
+// MongoDB connection URI
+let uri = ``;
+let client = new MongoClient(uri);
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Middleware
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', async (req, res) => {
+  res.send('Hello World!');
+});
+
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
+
+module.exports = app;
